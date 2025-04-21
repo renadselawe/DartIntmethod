8ec677dd3fa24cb40c0173338b9b4be615658233
renadselawe <renadselawe@gmail.com>
2025-04-21 20:21:56 +0300

    update

---
+++ main.dart
@@ -0,0 +1,13 @@
+void main (){
+  String myname = 'renad';
+  int myage = 26;
+  double hight = 166.4;
+  int age10= myage +10 ;
+  print (myname[0].toUpperCase());
+  print("myage is $myage");
+  print("my age after 10 years $age10");
+  print(hight.floor());
+  print(hight.ceil());
+  print(hight.round());
+  print(hight.isNegative);
+}
